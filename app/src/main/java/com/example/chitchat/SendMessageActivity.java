b37b9de180df2e802eccfd61fe1cb0a18d1c9bdc
Kaushal <kaushalkawade183@gmail.com>
2023-02-10 03:56:34 +0530

    ChitChat application is created

---
+++ app/src/main/java/com/example/chitchat/SendMessageActivity.java
@@ -0,0 +1,19 @@
+package com.example.chitchat;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class SendMessageActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_send_message);
+    }
+
+    public void onSendMessage(View view) {
+
+    }
+}
