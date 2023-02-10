135448de099afb9f908c091873118418cee9c93d
Kaushal <kaushalkawade183@gmail.com>
2023-02-10 06:27:38 +0530

    gridle modified

--- app/src/main/java/com/example/chitchat/SendMessageActivity.java
+++ app/src/main/java/com/example/chitchat/SendMessageActivity.java
@@ -2,10 +2,14 @@ package com.example.chitchat;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
-public class SendMessageActivity extends AppCompatActivity {
+public class SendMessageActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,6 +18,11 @@ public class SendMessageActivity extends AppCompatActivity {
     }
 
     public void onSendMessage(View view) {
+        TextView messageView = (TextView) findViewById(R.id.message);
+        String messageText = messageView.getText().toString();
 
+        Intent intent = new Intent(this,ReceiveMessageActivity.class);
+        intent.putExtra(ReceiveMessageActivity.EXTRA_MESSAGE,messageText);
+        startActivity(intent);
     }
 }
